8279b1cfe1bf3d0b08fb542cfbcc80a5a3e8d957
Eirik Gärtner <eirikgartner@gmail.com>
2022-09-26 21:59:53 +0200

    Added - configured header file xml_constexpr_string.hpp.in. - an example input for development use. - constexpr test for the configured file.

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch.hpp>
+#include <xml_constexpr_string.hpp>
 
 constexpr unsigned int Factorial(unsigned int number)// NOLINT(misc-no-recursion)
 {
@@ -11,4 +12,9 @@ TEST_CASE("Factorials are computed with constexpr", "[factorial]")
   STATIC_REQUIRE(Factorial(2) == 2);
   STATIC_REQUIRE(Factorial(3) == 6);
   STATIC_REQUIRE(Factorial(10) == 3628800);
+}
+
+TEST_CASE("File content is read at compile time.", "[constexpr_xml]")
+{
+  STATIC_REQUIRE(cexml::get_xml_input().starts_with('<'));
 }
