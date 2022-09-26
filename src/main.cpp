5e11ea28e67e2a82dd2d1e592782b1cb3f2f5b5c
Eirik Gärtner <eirikgartner@gmail.com>
2022-09-26 08:37:02 +0200

    Add dependency lexy with cmake fetchContent.

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,34 @@
 #include <optional>
 
 #include <CLI/CLI.hpp>
-#include <spdlog/spdlog.h>
+#include <lexy/callback.hpp>
+#include <lexy/dsl.hpp>
 
+#include <spdlog/spdlog.h>
 // This file will be generated automatically when you run the CMake configuration step.
 // It creates a namespace called `myproject`.
 // You can modify the source template at `configured_files/config.hpp.in`.
+#include "xml_parsing.hpp"
 #include <internal_use_only/config.hpp>
+namespace dsl = lexy::dsl;
+
+struct ipv4_address
+{
+  // What is being matched.
+  static constexpr auto rule = [] {
+    // Match a sequence of (decimal) digits and convert it into a std::uint8_t.
+    auto octet = dsl::integer<std::uint8_t>;
+
+    // Match four of them separated by periods.
+    return dsl::times<4>(octet, dsl::sep(dsl::period)) + dsl::eof;
+  }();
+
+  // How the matched output is being stored.
+  static constexpr auto value =
+    lexy::callback<std::uint32_t>([](std::uint8_t a, std::uint8_t b, std::uint8_t c, std::uint8_t d) {
+      return (a << 24) | (b << 16) | (c << 8) | d;
+    });
+};
 
 
 // NOLINTNEXTLINE(bugprone-exception-escape)
