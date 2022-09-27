7b736482db7e5baca91331099db9dbbac3768b33
Eirik Gärtner <eirikgartner@gmail.com>
2022-09-27 22:48:00 +0200

    Link lexy to constexpr_tests

--- test/constexpr_tests.cpp
+++ test/constexpr_tests.cpp
@@ -1,20 +1,31 @@
 #include <catch2/catch.hpp>
+#include <string_view>
+
 #include <xml_constexpr_string.hpp>
 
-constexpr unsigned int Factorial(unsigned int number)// NOLINT(misc-no-recursion)
-{
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
+#include "../src/xml_parsing.hpp"
+#include <lexy/action/parse.hpp>
+#include <lexy/callback.hpp>
+#include <lexy/dsl.hpp>
+#include <lexy_ext/compiler_explorer.hpp>
+#include <lexy_ext/report_error.hpp>
 
-TEST_CASE("Factorials are computed with constexpr", "[factorial]")
-{
-  STATIC_REQUIRE(Factorial(1) == 1);
-  STATIC_REQUIRE(Factorial(2) == 2);
-  STATIC_REQUIRE(Factorial(3) == 6);
-  STATIC_REQUIRE(Factorial(10) == 3628800);
-}
+using namespace std::string_view_literals;
 
 TEST_CASE("File content is read at compile time.", "[constexpr_xml]")
 {
-  STATIC_REQUIRE(cexml::get_xml_input().starts_with('<'));
+  STATIC_REQUIRE(cexml::get_xml_input().starts_with("123"sv));
+}
+
+TEST_CASE("Parsed at compile time!")
+{
+  auto sv = cexml::get_xml_input();
+  auto input = lexy::range_input(sv.begin(), sv.end());
+
+  auto result = lexy::parse<production::ipv4_address>(input, lexy_ext::report_error);
+  if (result) {
+    STATIC_REQUIRE(false);
+  } else {
+    STATIC_REQUIRE(true);
+  }
 }
